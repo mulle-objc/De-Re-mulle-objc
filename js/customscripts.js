b20e6b1b53abac9370b1aaef1b5dd4f35a17e645
Nat! <nat@mulle-kybernetik.com>
2019-03-27 11:51:51 +0100

    keep all files local for offline reading

--- js/customscripts.js
+++ js/customscripts.js
@@ -11,9 +11,9 @@ $( document ).ready(function() {
         $( "#mysidebar" ).attr("class", "nav affix");
     }
     // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
-    $('[data-toggle="tooltip"]').tooltip({
-        placement : 'top'
-    });
+    //    $('[data-toggle="tooltip"]').tooltip({
+    //    placement : 'top'
+    //});
 
     /**
      * AnchorJS
